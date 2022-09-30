cbcf7dc3dddb5dab8da38c5eafb378c61c2a9dd1
Christopher Salisbury <salischr@umich.edu>
2022-09-30 09:58:06 -0400

    Minor clean ups

--- pages/annarbor.py
+++ pages/annarbor.py
@@ -19,7 +19,7 @@ data_file_name = "export_data_domain_7992.xlsx"
 config_direction = {"in": "Northbound", 
                     "out": "Southbound"}
 loc_msg_markdown = "Location: N Division, Ann Arbor, MI ([Site photo](https://fenggroup.org/images/respic/bike-counter-a2division.png), [Google Maps](https://goo.gl/maps/1bcfHrqSYbqiRSXa8))"
-# dates_msg = "Data collection: X weeks (2022-08-26 to 2022-mm-dd)"
+dates_msg = 'Data collection: 2022-08-26 to 2022-09-25 (ongoing)'
 
 path = "./data/" + data_file_name
 df = pd.read_excel(path, names=["time", "in", "out"], skiprows=3)
@@ -32,10 +32,10 @@ df["time"] = pd.to_datetime(df["time"])
 df = df.set_index("time")
 
 # Set date range availability
-min_date_allowed = "2022-08-26"   # the first full *day* of data collection in AA
-# min_date_allowed = "2022-08-28"   # the first full *week* of data collection in AA
+min_date_allowed = '2022-08-26'   # the first full *day* of data collection in AA
+# min_date_allowed = '2022-08-28'   # the first full *week* of data collection in AA
 
-max_date_allowed = "2022-09-21"
+max_date_allowed = '2022-09-25'
 
 df = df[min_date_allowed : max_date_allowed]
 
@@ -65,14 +65,14 @@ figure_font = dict(family='Roboto',
 
 layout = html.Div([
     
-    html.Div(children=[ 
+    html.Div(children=[
              html.H3(children=dcc.Markdown(loc_msg_markdown)),
-            #  html.H3(children=dates_msg),
+             html.H3(children=dates_msg),
              ]),
     
-    html.Div(id="select-date-range",
+    html.Div(id='select-date-range',
              children=[
-             html.H3(children="Select dates"), 
+             html.H3(children='Select dates'), 
              dcc.DatePickerRange(id='my-date-picker-range',
                                  min_date_allowed = min_date_allowed,
                                  max_date_allowed = max_date_allowed,
@@ -82,33 +82,33 @@ layout = html.Div([
                                  minimum_nights=0,),
              ]),
     
-    html.Div(id="select-direction",
+    html.Div(id='select-direction',
              children=[
-             html.H3(children="Select traffic direction"), 
-             dcc.RadioItems(options={"bi_direction": "Both directions", 
-                                     "in": config_direction["in"], 
-                                     "out": config_direction["out"]},
-                            value="bi_direction",
+             html.H3(children='Select traffic direction'), 
+             dcc.RadioItems(options={'bi_direction': 'Both directions', 
+                                     'in': config_direction['in'], 
+                                     'out': config_direction['out']},
+                            value='bi_direction',
                             id='data-dir-radio'),
             ]),
     
-    html.Div(id="select-resolution",
+    html.Div(id='select-resolution',
              children=[
-             html.H3(children="Select data resolution"), 
-             dcc.RadioItems(options={"1_day": "1 day",
-                                     "1_hour": "1 hour", 
-                                     "30_min": "30 min",
-                                     "15_min": "15 min"}, 
-                            value="1_day",
+             html.H3(children='Select data resolution'), 
+             dcc.RadioItems(options={'1_week': 'weekly',
+                                     '1_day': 'daily',
+                                     '1_hour': 'hourly', 
+                                     '15_min': '15 min'}, 
+                            value='1_day',
                             id='data-agg-radio'),
             ]),
 
     html.Div(children=[
-    dcc.Graph(id='bar-graph-annarbor',style={"margin-top": "20px", "margin-bottom": "40px"})]),
+    dcc.Graph(id='bar-graph-aa',style={'margin-top': '20px', 'margin-bottom': '40px'})]),
 
     html.Div(children=[
              html.H3(children='Traffic summary on the selected dates'), 
-             dash_table.DataTable(data = table.to_dict("records"), 
+             dash_table.DataTable(data = table.to_dict('records'), 
                                   columns =[
                                     dict(id='dir', name=''),
                                     dict(id='total_vol', name='Total traffic', type='numeric', format=dash_table.Format.Format().group(True)),
@@ -127,23 +127,23 @@ layout = html.Div([
                                         ],
                                   style_table={'height': '250px', 'overflowY': 'auto'},
                                   style_cell={'font-family':'Roboto', 'padding-right': '10px', 'padding-left': '10px'},
-                                  id='avg-table-annarbor'),
+                                  id='avg-table-aa'),
             ]), 
     
-    html.Div(children=[dcc.Graph(id='time-of-day-annarbor', style={"margin-top": "-80px", "margin-bottom": "50px"})]),
-    html.Div(children=[dcc.Graph(id='day-of-week-annarbor')]),
+    html.Div(children=[dcc.Graph(id='time-of-day-aa', style={'margin-top': '-80px', 'margin-bottom': '50px'})]),
+    html.Div(children=[dcc.Graph(id='day-of-week-aa')]),
 
     html.Div(children=[
-        html.H4(children=dcc.Markdown("Download the [data files](https://github.com/fenggroup/bike-traffic-plotly-dash/tree/main/data)")),
-        html.H4(children=dcc.Markdown("[Click here](https://fenggroup.org/bike-counter/) to learn more about our bike counting project.")), 
-        html.H4(children=dcc.Markdown("This dashboard is open source and hosted on [our GitHub repository](https://github.com/fenggroup/bike-traffic-plotly-dash).")), 
-        html.H4(children=dcc.Markdown("[Feng Group](https://fenggroup.org/) 2022"))
+        html.H4(children=dcc.Markdown('Download the [data files](https://github.com/fenggroup/bike-traffic-plotly-dash/tree/main/data)')),
+        html.H4(children=dcc.Markdown('[Click here](https://fenggroup.org/bike-counter/) to learn more about our bike counting project.')), 
+        html.H4(children=dcc.Markdown('This dashboard is open source and hosted on [our GitHub repository](https://github.com/fenggroup/bike-traffic-plotly-dash).')), 
+        html.H4(children=dcc.Markdown('[Feng Group](https://fenggroup.org/) 2022'))
     ])
     
 ])
 
 @callback(
-    Output(component_id='bar-graph-annarbor', component_property='figure'),
+    Output(component_id='bar-graph-aa', component_property='figure'),
     Input(component_id='data-dir-radio', component_property='value'),
     Input(component_id='data-agg-radio', component_property='value'),
     Input(component_id='my-date-picker-range', component_property='start_date'),
@@ -151,37 +151,37 @@ layout = html.Div([
 
 def update_figure(dir_radio_val, agg_radio_val, start_date, end_date):
 
-    if agg_radio_val == "15_min":
-        rule = "15T"
-    elif agg_radio_val == "30_min":
-        rule = "30T"
-    elif agg_radio_val == "1_hour":
-        rule = "H"
-    elif agg_radio_val == "1_day":
-        rule = "D"
+    if agg_radio_val == '15_min':
+        rule = '15T'
+    elif agg_radio_val == '1_hour':
+        rule = 'H'
+    elif agg_radio_val == '1_day':
+        rule = 'D'
+    elif agg_radio_val == '1_week':
+        rule = 'W'
 
     df_updated = df_update(df=df, rule=rule, start_date=start_date, end_date=end_date)  
     
-    if dir_radio_val == "in":
+    if dir_radio_val == 'in':
 
-        hover_data= ["day_of_week"]
+        hover_data= ['day_of_week']
         marker_color = color_in
     
-    elif dir_radio_val == "out":
+    elif dir_radio_val == 'out':
 
-        hover_data= ["day_of_week"]
+        hover_data= ['day_of_week']
         marker_color = color_out
 
-    elif dir_radio_val == "bi_direction":
+    elif dir_radio_val == 'bi_direction':
 
-        hover_data = ["in", "out", "day_of_week"]
+        hover_data = ['in', 'out', 'day_of_week']
         marker_color = color_both_direction
     
-    labels = {"time": "Date", 
-              "bi_direction": "Total",
-              "in": config_direction["in"],
-              "out": config_direction["out"],
-              "day_of_week": "Day of week"}
+    labels = {'time': 'Date', 
+              'bi_direction': 'Total',
+              'in': config_direction['in'],
+              'out': config_direction['out'],
+              'day_of_week': 'Day of week'}
 
     fig1 = px.bar(df_updated, 
                   x=df_updated.index, 
@@ -192,71 +192,71 @@ def update_figure(dir_radio_val, agg_radio_val, start_date, end_date):
 
     fig1.update_traces(marker_color=marker_color)
 
-    fig1.update_layout(xaxis_title="Date & time", 
-                       yaxis_title="Count", 
-                       title="<b>Bike traffic counts by date & time</b>",
+    fig1.update_layout(xaxis_title='Date & time', 
+                       yaxis_title='Count', 
+                       title='<b>Bike traffic counts by date & time</b>',
                        title_x=0.5,  # center title
                        transition_duration=500, 
                        font=figure_font,
-                       hoverlabel=dict(font_color="white"))
+                       hoverlabel=dict(font_color='white'))
        
     # number of days in the selected date range
     numdays = (df_updated.index.max() - df_updated.index.min()).days
     
     # Disable showing time of day on x-axis in the daily bar chat.
     # instead, show everyday once (delta tick of 1 day)
-    if (agg_radio_val == "1_day") and (numdays <= 7):
-        fig1.update_xaxes(dtick="d1", tickformat="%b%e\n%Y")
+    if (agg_radio_val == '1_day') and (numdays <= 7):
+        fig1.update_xaxes(dtick='d1', tickformat='%b%e\n%Y')
     
     return fig1
     
 
 @callback(
-    Output(component_id='avg-table-annarbor', component_property='data'),
+    Output(component_id='avg-table-aa', component_property='data'),
     Input(component_id='my-date-picker-range', component_property='start_date'),
     Input(component_id='my-date-picker-range', component_property='end_date'))
 
 def update_table(start_date, end_date):
 
     df_updated = df_update(df=df, 
-                           rule="D", 
+                           rule='D', 
                            start_date=start_date, 
                            end_date=end_date)
 
-    df_updated.drop(columns="day_of_week", inplace=True)
+    df_updated.drop(columns='day_of_week', inplace=True)
 
     total_vol = df_updated.sum()
     daily_avg = df_updated.mean()
 
     # Calculate percentages
-    perc_in = total_vol.loc["in"] / total_vol.loc["bi_direction"]
-    perc_out = total_vol.loc["out"] / total_vol.loc["bi_direction"]
+    perc_in = total_vol.loc['in'] / total_vol.loc['bi_direction']
+    perc_out = total_vol.loc['out'] / total_vol.loc['bi_direction']
 
-    perc = pd.Series({"in": perc_in, 
-                      "out": perc_out, 
-                      "bi_direction": 1})
+    perc = pd.Series({'in': perc_in, 
+                      'out': perc_out, 
+                      'bi_direction': 1})
 
-    direction = pd.Series({"in": config_direction["in"], 
-                           "out": config_direction["out"], 
-                           "bi_direction": "Both directions"})
+    direction = pd.Series({'in': config_direction['in'], 
+                           'out': config_direction['out'], 
+                           'bi_direction': 'Both directions'})
 
     table = pd.DataFrame((direction, total_vol, daily_avg, perc)).T
 
-    table.columns = ["dir", "total_vol", "daily_avg", "perc"]
+    table.columns = ['dir', 'total_vol', 'daily_avg', 'perc']
 
-    data_table = table.loc[["bi_direction", "in", "out"]].to_dict("records")
+    data_table = table.loc[['bi_direction', 'in', 'out']].to_dict('records')
     
     return  data_table
 
 @callback(
-    Output(component_id='time-of-day-annarbor', component_property='figure'),
+    Output(component_id='time-of-day-aa', component_property='figure'),
     Input(component_id='data-dir-radio', component_property='value'),
     Input(component_id='my-date-picker-range', component_property='start_date'),
     Input(component_id='my-date-picker-range', component_property='end_date'))
 
 def update_figure(dir_radio_val, start_date, end_date):
    
-    df_time = df_update(df=df, rule="15T", start_date=start_date, end_date=end_date)
+    df_time = df_update(df=df, rule='15T', start_date=start_date, end_date=end_date)
        
     ctb_time = pd.crosstab(index = [df_time.index.isocalendar().week, df_time.index.day], 
                            columns  =df_time.index.hour,
@@ -264,8 +264,8 @@ def update_figure(dir_radio_val, start_date, end_date):
                            values = df_time[dir_radio_val], 
                            aggfunc = 'sum')
     
-    labels = {"col_0": "Time of day", 
-              "value": "Count"}
+    labels = {'col_0': 'Time of day', 
+              'value': 'Count'}
 
     fig2 = px.box(data_frame=ctb_time,
                   labels=labels, 
@@ -284,15 +284,15 @@ def update_figure(dir_radio_val, start_date, end_date):
 
     alpha = 0.4
 
-    if dir_radio_val == "in":
+    if dir_radio_val == 'in':
 
         marker_color = rgb2rgba(color_in, alpha)
     
-    elif dir_radio_val == "out":
+    elif dir_radio_val == 'out':
 
         marker_color = rgb2rgba(color_out, alpha)
 
-    elif dir_radio_val == "bi_direction":
+    elif dir_radio_val == 'bi_direction':
 
         marker_color = rgb2rgba(color_both_direction, alpha)
     
@@ -300,9 +300,9 @@ def update_figure(dir_radio_val, start_date, end_date):
                        marker_size=10,
                        jitter=0.7)
 
-    fig2.update_layout(xaxis_title="Time of day", 
-                       yaxis_title="Count",
-                       title="<b>Hourly traffic by time of day</b>",
+    fig2.update_layout(xaxis_title='Time of day', 
+                       yaxis_title='Count',
+                       title='<b>Hourly traffic by time of day</b>',
                        title_x=0.5,  # center title
                        transition_duration=500,
                        font=figure_font,
@@ -313,14 +313,14 @@ def update_figure(dir_radio_val, start_date, end_date):
     return fig2
 
 @callback(
-    Output(component_id='day-of-week-annarbor', component_property='figure'),
+    Output(component_id='day-of-week-aa', component_property='figure'),
     Input(component_id='data-dir-radio', component_property='value'),
     Input(component_id='my-date-picker-range', component_property='start_date'),
     Input(component_id='my-date-picker-range', component_property='end_date'))
 
 def update_figure(dir_radio_val, start_date, end_date):
     
-    df_day = df_update(df=df, rule="15T", start_date=start_date, end_date=end_date)
+    df_day = df_update(df=df, rule='15T', start_date=start_date, end_date=end_date)
 
 
     ctb_day = pd.crosstab(index=df_day.index.isocalendar().week,
@@ -330,27 +330,27 @@ def update_figure(dir_radio_val, start_date, end_date):
 
     category_orders = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-    labels = {"day_of_week": "Day of week", 
-              "value": "Count"}
+    labels = {'day_of_week': 'Day of week', 
+              'value': 'Count'}
 
     fig3 = px.box(data_frame=ctb_day, 
                     category_orders=category_orders,
                     labels=labels,
-                    hover_data=["day_of_week"], 
+                    hover_data=['day_of_week'], 
                     template=template, 
-                    points="all")
+                    points='all')
 
     alpha = 0.7
 
-    if dir_radio_val == "in":
+    if dir_radio_val == 'in':
 
         marker_color = rgb2rgba(color_in, alpha)
     
-    elif dir_radio_val == "out":
+    elif dir_radio_val == 'out':
 
         marker_color = rgb2rgba(color_out, alpha)
 
-    elif dir_radio_val == "bi_direction":
+    elif dir_radio_val == 'bi_direction':
 
         marker_color = rgb2rgba(color_both_direction, alpha)
 
@@ -358,9 +358,9 @@ def update_figure(dir_radio_val, start_date, end_date):
                        marker_size=20,
                        jitter=0.3)
 
-    fig3.update_layout(xaxis_title="Day of week", 
-                       yaxis_title="Count",
-                       title="<b>Daily traffic by day of week</b>",
+    fig3.update_layout(xaxis_title='',
+                       yaxis_title='Count',
+                       title='<b>Daily traffic by day of week</b>',
                        title_x=0.5,  # center title
                        yaxis_range=[0, ctb_day.max().max()+20], 
                        transition_duration=500,
