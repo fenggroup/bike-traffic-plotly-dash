6115a86697ffdef7d104d536eb63710912848ed7
Fred Feng <fredfeng@umich.edu>
2022-10-25 14:35:57 -0400

    Improve weather chart, add weather data to chart hovers, other misc minor improvements.

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def df_update(df, rule, start_date, end_date):
     return df_filtered
 
 # reads the temperature data into the dash
-df_temp=pd.read_csv("./data/weather-noaa-annarbor.csv")
+df_temp = pd.read_csv("./data/weather-noaa-annarbor.csv")
 
 # Convert the time to a pandas datetime object
 df_temp["DATE"] = pd.to_datetime(df_temp["DATE"])
@@ -103,6 +103,7 @@ def call_layout():
     layout =  html.Div([
         
         html.Div(children=[
+                html.H1(children=title), 
                 html.H3(children=dcc.Markdown(site_config['loc_msg_markdown'])),
                 html.H3(children=site_config['dates_msg']),
                 ]),
@@ -151,7 +152,7 @@ def call_layout():
                                     columns =[
                                         dict(id='dir', name=''),
                                         dict(id='total_vol', name='Total traffic', type='numeric', format=dash_table.Format.Format().group(True)),
-                                        dict(id='daily_avg', name='Ave. daily traffic', type='numeric', format=dash_table.Format.Format(precision=1, scheme=dash_table.Format.Scheme.fixed)),
+                                        dict(id='daily_avg', name='Average daily traffic', type='numeric', format=dash_table.Format.Format(precision=1, scheme=dash_table.Format.Scheme.fixed)),
                                         dict(id='perc', name='Percent', type='numeric', format=dash_table.FormatTemplate.percentage(1))
                                         ],
                                         style_cell_conditional=[
@@ -169,16 +170,20 @@ def call_layout():
                                     id='avg-table'),
                 ]), 
         
-        html.Div(children=[dcc.Graph(id='avg-hour-traffic', style={'margin-top': '-80px', 'margin-bottom': '50px'},
+
+        html.Div(children=[dcc.Graph(id='time-of-day', style={'margin-top': '-100px', 'margin-bottom': '20px'},
+                                config = {'toImageButtonOptions': {'format': 'png','filename': 'time_of_day_chart', 'height': 350,'width': 750,'scale': 10}})]),        
+
+        html.Div(children=[dcc.Graph(id='avg-hour-traffic', style={'margin-bottom': '20px'},   
                                 config = {'toImageButtonOptions': {'format': 'png','filename': 'avg_hourly_traffic_chart', 'height': 350,'width': 750,'scale': 10}})]),
-        html.Div(children=[dcc.Graph(id='time-of-day',
-                                config = {'toImageButtonOptions': {'format': 'png','filename': 'time_of_day_chart', 'height': 350,'width': 750,'scale': 10}})]),
-        html.Div(children=[dcc.Graph(id='day-of-week',
+
+        html.Div(children=[dcc.Graph(id='day-of-week', style={'margin-bottom': '20px'},   
                                 config = {'toImageButtonOptions': {'format': 'png','filename': 'day_of_week_chart', 'height': 350,'width': 750,'scale': 10}})]),
         
-        html.Div(children=[dcc.Checklist(id='day-checklist',                      
+        html.Div(children=[dcc.Checklist(id='day-checklist',          
                                 options=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
                                 value=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])]),
+
         html.Div(children=[dcc.Graph(id='weather-plot',
                                 config = {'toImageButtonOptions': {'format': 'png','filename': 'weather_chart', 'height': 350,'width': 750,'scale': 10}})]),
 
@@ -219,9 +224,9 @@ def display_page(pathname):
                            config_direction={'in':'Northbound', 
                                              'out':'Southbound'}, 
                            loc_msg_markdown='Location: N Division, Ann Arbor, MI ([Site photo](https://fenggroup.org/images/respic/bike-counter-a2division.png), [Google Maps](https://goo.gl/maps/1bcfHrqSYbqiRSXa8))',
-                           dates_msg='Data collection: 2022-08-26 to 2022-10-15 (ongoing)', 
+                           dates_msg='Data collection: 2022-08-26 to 2022-10-24 (ongoing)', 
                            date_range=['2022-08-26',   # the first full *day* of data collection in AA
-                                       '2022-10-15'],
+                                       '2022-10-24'],
                            )
 
     return call_layout()
@@ -247,8 +252,16 @@ def update_figure(dir_radio_val, agg_radio_val, start_date, end_date):
     elif agg_radio_val == '1_week':
         rule = 'W'
 
-    df_updated = df_update(df=df, rule=rule, start_date=start_date, end_date=end_date)  
-    
+    df_updated = df_update(df=df, rule=rule, start_date=start_date, end_date=end_date)
+
+
+    labels = {'time': 'Date', 
+              'bi_direction': 'Total count',
+              'in': site_config['config_direction']['in'],
+              'out': site_config['config_direction']['out'],
+              'day_of_week': 'Day of week',
+              }
+
     if dir_radio_val == 'in':
 
         hover_data= ['day_of_week']
@@ -263,12 +276,19 @@ def update_figure(dir_radio_val, agg_radio_val, start_date, end_date):
 
         hover_data = ['in', 'out', 'day_of_week']
         marker_color = color_both_direction
-    
-    labels = {'time': 'Date', 
-              'bi_direction': 'Total',
-              'in': site_config['config_direction']['in'],
-              'out': site_config['config_direction']['out'],
-              'day_of_week': 'Day of week'}
+
+
+    # add features for *daily* chart
+    if agg_radio_val == '1_day':
+
+        df_updated = df_updated.join(df_temp)
+
+        labels.update({'TMAX': 'Temperature high (F)',
+                       'TMIN': 'Temperature low (F)',
+                       'PRCP': 'Precipitation (inches)',
+                      })
+
+        hover_data = hover_data + ['TMIN', 'TMAX', 'PRCP']
 
     fig1 = px.bar(df_updated, 
                   x=df_updated.index, 
@@ -485,14 +505,16 @@ def update_figure(dir_radio_val, start_date, end_date):
     fig4 = px.line(ctb_time.T,
                        labels = labels)
 
-    xticks=np.arange(-0.5, 24, 1) 
-    xlabels=np.arange(0, 25, 1)
+    xticks = np.arange(-0.5, 24, 1) 
+    xlabels = np.arange(0, 25, 1)
 
     fig4['layout'] = {'xaxis':{'tickvals':xticks, 
                       'ticktext':xlabels,
                       'showline':True
                             }
                       }
+
+    fig4.update_traces(line=dict(width=4))
 
     fig4.update_layout(xaxis_title='Time of day', 
                        yaxis_title='Count',
@@ -505,15 +527,27 @@ def update_figure(dir_radio_val, start_date, end_date):
                        template=template,
                        modebar_remove=['zoom', 'pan', 'select','lasso2d', 'zoomIn', 'zoomOut', 'autoScale'])
     
+    # add annotation
+    fig4.add_annotation(dict(font=dict(color='gray',size=18),
+                                            x=1.08,
+                                            y=1.1,
+                                            showarrow=False,
+                                            text="You can click on a particular day-of-week in the legend to show/hide the data ↓",
+                                            textangle=0,
+                                            xanchor='right',
+                                            xref="paper",
+                                            yref="paper"))
+
     return fig4
 
 @callback(
     Output(component_id='weather-plot', component_property='figure'),
+    Input(component_id='data-dir-radio', component_property='value'),
     Input(component_id='day-checklist', component_property='value'),
     Input(component_id='my-date-picker-range', component_property='start_date'),
     Input(component_id='my-date-picker-range', component_property='end_date'))
 
-def update_figure(day_checklist_val, start_date, end_date):
+def update_figure(dir_radio_val, day_checklist_val, start_date, end_date):
 
     df_updated = df_update(df=df, rule='D', start_date=start_date, end_date=end_date)
 
@@ -524,17 +558,39 @@ def update_figure(day_checklist_val, start_date, end_date):
     df_weather = df_weather[df_weather['day_of_week'].isin(day_checklist_val)]
 
     labels = {'bi_direction': 'Daily count', 
-              'value': 'Temperature'}
+              'value': 'Temperature',
+              'TMAX': 'Maximum temperature (F)',
+              'day_of_week': 'Day of week',
+              'PRCP': 'Precipitation (inches)'}
 
-    hover_data= ['day_of_week']
+
+    hover_data= ['day_of_week', 'PRCP']
 
     fig5 = px.scatter(df_weather, 
                       x='TMAX', 
-                      y='bi_direction', 
+                      y=dir_radio_val,
                       labels = labels,
-                      hover_data=hover_data)
+                      hover_data=hover_data, 
+                      trendline='ols')
 
-    fig5.update_layout(xaxis_title='Temperature (degrees F)', 
+    alpha = 0.7
+
+    if dir_radio_val == 'in':
+
+        marker_color = rgb2rgba(color_in, alpha)
+    
+    elif dir_radio_val == 'out':
+
+        marker_color = rgb2rgba(color_out, alpha)
+
+    elif dir_radio_val == 'bi_direction':
+
+        marker_color = rgb2rgba(color_both_direction, alpha)
+
+    fig5.update_traces(marker_color=marker_color, 
+                       marker_size=20)
+
+    fig5.update_layout(xaxis_title='Maximum temperature (Fahrenheit)', 
                        yaxis_title='Daily count',
                        title='<b>Daily traffic by maximum temperature</b>',
                        title_x=0.5,  # center title
