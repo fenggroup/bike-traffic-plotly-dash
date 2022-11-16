737bee1e8497e31bf87989d54496da3a154c1976
Fred Feng <fredfeng@umich.edu>
2022-11-16 13:06:03 -0500

    Update Ann Arbor data.

--- sites.py
+++ sites.py
@@ -8,9 +8,9 @@ site_01 = dict(site_url='/annarbor-1',
                config_direction={'in': 'Northbound',
                                  'out': 'Southbound'},
                loc_msg_markdown='[Ann Arbor Division Street Bikeway](https://www.a2dda.org/people-friendly-streets/projects/division-street-bikeway-project/) | [Site photo](https://fenggroup.org/images/respic/bike-counter-a2division.png) | [Site location](https://goo.gl/maps/1bcfHrqSYbqiRSXa8)',
-               dates_msg='Data collection: 2022-08-26 to 2022-11-10 (ongoing)',
+               dates_msg='Data collection: 2022-08-26 to 2022-11-15 (ongoing)',
                date_range=['2022-08-26',   # the first full *day* of data collection
-                           '2022-11-10'],
+                           '2022-11-15'],
                default_res='1_day',
               )
 
